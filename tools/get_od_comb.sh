c7a87f5c322a7c332e60c8459f94b93a9a40d146
jonseijo <jon.seijo@gmail.com>
2017-11-13 11:26:26 -0300

    Arregla script de la catedra

--- tools/get_od_comb.sh
+++ tools/get_od_comb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Filtramos todas las posibles combinaciones origen-destino. 
+# Filtramos todas las posibles combinaciones origen-destino.
 
 # Definimos algunas variables. Ojo que a bash no le gustan los espacios cerca del "=".
 year="2008"
 infile="../data/2008.csv"
 outfile="od_comb_2008.csv"
 
-cut -f17,18 -d, $infile | sort | uniq > $outfile
+cut -f17,18 -d, $infile | tail -n +2 | sort | uniq > $outfile
