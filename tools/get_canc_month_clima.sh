95d98274a2aa1a625dd304ba39b2015311c3a943
jonseijo <jon.seijo@gmail.com>
2017-11-13 12:59:43 -0300

    Script re piola para obtener cancelaciones por categoria

--- tools/get_canc_month_clima.sh
+++ tools/get_canc_month_clima.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 # Buscamos numero de vuelos cancelados por mes entre dos aeropuertos.
 
-# Definimos algunas variables. Ojo que a bash no le gustan los espacios cerca del "=".
-year="2005"
-infile="../data/""$year"".csv"
-outfile="cancelaciones_mes_clima-""$year"".csv"
-
-# La secuencia es:
-# 1. Primero filtramos por vuelos cancelados. Esto lo hacemos una unica vez y lo guardamos en el archivo canc_aux.csv
-# 2. Del output de 1, filtramos las lineas que matchean el mes. Esto es facil de hacer con el comando grep.
-# 3. Contamos la cantidad de lineas con el comando wc.
-# El resultado queda en $outfile, es un vector con las cancelaciones por mes del año elegido.
-
-
 # MIA: Miami
+# MSY: Louis Armstrong New Orleans International Airport
 # MCO: Orlando
 # DAB: Daytona Beach
 # ECP: Panama city
 
-#     destino MIAMI cancelados por clima
-awk -F, '$18=="MIA" && $22=="1" && $23=="B"' $infile > canc_aux.csv
-for i in `seq 1 12`; do cat canc_aux.csv | grep $year,$i | wc -l; done > $outfile;
 
-# Borramos el archivo auxiliar.
-rm canc_aux.csv
+outfile="cancelaciones_mes_clima.csv"
+echo "Year,Month,Cancelaciones" > $outfile
+
+# for year in `seq 2000 2008`; do
+for year in `seq 2007 2008`; do
+
+	infile="../data/""$year"".csv"
+
+	# con WeatherDelay
+	# awk -F, '!($26=="0")' $infile > canc_aux.csv
+
+	# cancelados
+	# awk -F, '$22=="1"' $infile > canc_aux.csv
+
+	# cancelados por clima
+	awk -F, '$22=="1" && $23=="B"' $infile > canc_aux.csv
+
+	# cancelados por clima hacia destino X ($18 == .. )
+	# awk -F, '($18=="MSY" || $18=="MIA" || $18=="MCO") && $22=="1" && $23=="B"' $infile > canc_aux.csv
+
+	for i in `seq 1 12`; do
+		cat canc_aux.csv | grep $year,$i | echo $year","`wc -l`
+	done >> $outfile;
+
+	rm canc_aux.csv
+
+done
